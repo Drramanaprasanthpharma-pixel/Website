fd4b22e5d379fb44220de252ce5962fdcc567268
Drramanaprasanthpharma-pixel <ramanaprasanthpharma@gmail.com>
2026-08-14 17:47:43 +0530

    Create supabase.js

---
+++ supabase.js
@@ -0,0 +1,7 @@
+const SUPABASE_URL = "https://zgphixxavlifdvlvmrbf.supabase.co";
+const SUPABASE_KEY = "sb_publishable_ncFJfHuFFuPRJpO5tBbh6w_7Oq9HsG8";
+
+const supabaseClient = supabase.createClient(
+  SUPABASE_URL,
+  SUPABASE_KEY
+);
